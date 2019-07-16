b71c2871923b6cafb973bfc6db9d061bf4152b7c
Maryana Marinho <maryanalirius@hotmail.com>
2019-07-16 18:29:14 -0300

    Validacao customizada: email nao repetido na atualizacao de Cliente

--- src/main/java/com/maryana/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/maryana/cursomc/dto/ClienteDTO.java
@@ -1,12 +1,14 @@
 package com.maryana.cursomc.dto;
 
 import com.maryana.cursomc.domain.Cliente;
+import com.maryana.cursomc.services.validation.ClienteUpdate;
 import org.hibernate.validator.constraints.Length;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 import java.io.Serializable;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 
     private static final long serialVersionUID = 1L;

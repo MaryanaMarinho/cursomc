20c55ede8a4b79b8b8babc820ad03fa11d33092c
Maryana Marinho <maryanalirius@hotmail.com>
2019-07-19 18:54:13 -0300

    Email HTML

--- src/main/java/com/maryana/cursomc/services/EmailService.java
+++ src/main/java/com/maryana/cursomc/services/EmailService.java
@@ -3,10 +3,16 @@ package com.maryana.cursomc.services;
 import com.maryana.cursomc.domain.Pedido;
 import org.springframework.mail.SimpleMailMessage;
 
+import javax.mail.internet.MimeMessage;
+
 
 public interface EmailService {
 
     void sendOrderConfirmationEmail(Pedido obj);
 
     void sendEmail(SimpleMailMessage msg);
+
+    void sendOrderConfirmationHtmlEmail(Pedido obj);
+
+    void sendHtmlEmail(MimeMessage msg);
 }
